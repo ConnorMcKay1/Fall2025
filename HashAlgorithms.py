d5feeb9dc4d54d8ee197852bc305f1fc4b7a605a
Connor <mckayc1@go.stockton.edu>
2025-11-20 20:28:34 -0500

    Prints out list of rockyou.txt in SHA-1 hash format

--- HashAlgorithms.py
+++ HashAlgorithms.py
@@ -1,15 +1,36 @@
+from ListReader import *
 import hashlib
 
 print("yippy " + '\n')
 
+HashedPasswordList = []
 
 #hashes a word/password
-h = hashlib.new('sha1')
-h.update(b"Nobody")
-print(h.hexdigest())
-
+def HashAlgorithm(password):
+    h = hashlib.new('sha1')
+    h.update(password.encode('utf-8'))
+    return h.hexdigest()
 
 #run a dictionary/rainbow list against the word that is hashed above
 #-take the "rockyou.txt" list and hash every password
 #-check those hashed passwords from rockyou.txt and compare it to the hashed password
 #   in this file
+
+def BruteForce(PasswordList):
+    for password in PasswordList:
+        hashedPassword = HashAlgorithm(password)
+        HashedPasswordList.append(hashedPassword)
+
+
+
+TxtReader()  #populates the list in ListReader
+
+HashAlgorithm("\nbrazil\n")
+
+BruteForce(PasswordList[:100])   #limits hashing to n
+
+print("\nFirst 100 hashed passwords:\n")
+for item in HashedPasswordList[:100]:
+    print(item)
+
+
--- ListReader.py
+++ ListReader.py
@@ -3,37 +3,39 @@ print("hellow" + '\n')
 PasswordList = []
 PasswordLengthList = []
 
-#word.replace('\n','') for word in f
-with open("rockyou.txt", "r", encoding="utf-8", errors="ignore") as f:
-    count = 0
-    for word in f:
-        processed_word = word.rstrip('\n')
-        count = count + 1
-        #print(processed_word)
-        PasswordList.append(processed_word)
+def TxtReader():
+    with open("rockyou.txt", "r", encoding="utf-8", errors="ignore") as f:
+        count = 0
+        for word in f:
+            processed_word = word.rstrip('\n')
+            count += 1
+            #print(processed_word)
+            PasswordList.append(processed_word)
     print('\n' + "number of words:")
     print(count)
     #print(PasswordList)
 
 
-def WordLength(Password_List, PasswordLengthList):
-    for word in Password_List:
-        PasswordLengthList.append(len(word) - 1)
-    return PasswordLengthList
+
+    #gets the length of every password in the .txt list
+# def WordLength(Password_List, PasswordLengthList):
+#     for word in Password_List:
+#         PasswordLengthList.append(len(word) - 1)
+#     return PasswordLengthList
 
 
 
-# For somet reason this is the varaible that the visualization class uses
-# in the graphs
+    # For somet reason this is the varaible that the visualization class uses
+    # in the graphs
 #
-WordLength(PasswordList, PasswordLengthList)    #gets the character count for each password and makes a list of of them 
+# WordLength(PasswordList, PasswordLengthList)    #gets the character count for each password and makes a list of of them 
 #print(PasswordLengthList)
 print("-----------------")
-NumberOfPasswords = PasswordList.__len__()
-print(NumberOfPasswords)
+# NumberOfPasswords = PasswordList.__len__()
+# print(NumberOfPasswords)
 print("-----------------")
-MaxPasswordLength = max(PasswordLengthList)
-print(MaxPasswordLength)
+# MaxPasswordLength = max(PasswordLengthList)     #finds the max lenght password in the .txt file
+# print(MaxPasswordLength)
 
 
 
